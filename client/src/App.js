fe4b547ce48ff043de6514017504a59f1f407110
yunjae228 <yvkqzhs7@gmail.com>
2021-08-27 18:46:21 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 
@@ -20,6 +21,9 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 });
 
@@ -29,11 +33,13 @@ class App extends Component {
 
   //state는 component내에서 변경될 수 있는 변수를 처리할 때 사용.
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   //api 서버에 접근해서 데이터를 받아올 때 사용
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20) ;
     this.callApi()
       .then(res => this.setState({customers : res}))
       .catch(err => console.log(err));
@@ -43,6 +49,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } =this.state;
+    this.setState({ completed: completed >=100 ? 0 : completed + 1});
   }
 
   render() {
@@ -74,7 +85,12 @@ class App extends Component {
             gender={c.gender}
             job={c.job}
           />);
-        }) : ""
+        }) : 
+        <TableRow>
+          <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant = "determinate" value = {this.state.completed} />
+          </TableCell>
+        </TableRow>
         }
         </TableBody>
       </Table>
