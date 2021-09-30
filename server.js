fb64f0cd31c5af0b36effecd500209b7f766eae8
yunjae228 <yvkqzhs7@gmail.com>
2021-09-30 17:52:09 +0900

    Add CustomerDelete Module

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const upload = multer({dest: './upload'});
 
 app.get('/api/customers', (req, res ) => {
     connection.query(
-      "SELECT * FROM management.CUSTOMER",
+      "SELECT * FROM management.CUSTOMER WHERE isDeleted = 0",
       (err, rows, fields) => {
         res.send(rows);
         
@@ -37,7 +37,7 @@ app.get('/api/customers', (req, res ) => {
 app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
-    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(),0)';
     let image = '/image/' + req.file.filename;
     let name = req.body.name;
     let birthday = req.body.birthday;
@@ -51,6 +51,15 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
       
       );
 
+});
+
+app.delete('/api/customers/:id', (req,res) => {
+    let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+    let params = [req.params.id];
+    connection.query(sql, params, 
+      (err, rows, fields) => {
+        res.send(rows);
+      })
 });
 
 app.listen(port, () => console.log('listening on port ' + port ));
